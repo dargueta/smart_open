b6f5e847f4a398b14e01eaadc14c263536cd701c
Diego Argueta <620513-dargueta@users.noreply.github.com>
2020-03-23 22:20:47 -0700

    Start implementing plugin detection

--- smart_open/plugins.py
+++ smart_open/plugins.py
@@ -1,42 +1,30 @@
 from __future__ import absolute_import
 
-import pluggy
 import typing
+import warnings
+
+import pkg_resources
+import pluggy
+import six
 
 
 if typing.TYPE_CHECKING:
+    from typing import Any
     from typing import BinaryIO
-    from typing import Iterable
-    from typing import Iterator
-    from typing import Union
+    from typing import Dict
     from smart_open.smart_open_lib import Uri
     from smart_open.io_utils import IOMode
 
 
-_protocol_hookspec = pluggy.HookspecMarker("smart_open_protocol")
-protocol_hook = pluggy.HookimplMarker("smart_open_protocol")
-
-_compression_hookspec = pluggy.HookspecMarker("smart_open_compression")
-compression_hook = pluggy.HookimplMarker("smart_open_compression")
+_hookspec = pluggy.HookspecMarker("smart_open")
+hook = pluggy.HookimplMarker("smart_open")
 
 
 class SmartOpenFileSystemPlugin(object):
     """Base class for all plugins implementing a file system protocol."""
 
-    @_protocol_hookspec
-    def smart_open_implemented_protocols(self):
-        # type: () -> Union[str, Iterable[str]]
-        """Declare what protocols this plugin implements.
-
-        Returns
-        -------
-
-        A string or iterable of strings naming the URI protocols that this plugin
-        implements.
-        """
-
-    @_protocol_hookspec
-    def smart_open_supports_read(self, protocol):
+    @_hookspec
+    def supports_read(self, protocol):
         # type: (str) -> bool
         """Does this plugin support reading operations for the given protocol?
 
@@ -56,8 +44,8 @@ class SmartOpenFileSystemPlugin(object):
         given protocol.
         """
 
-    @_protocol_hookspec
-    def smart_open_supports_write(self, protocol):
+    @_hookspec
+    def supports_write(self, protocol):
         # type: (str) -> bool
         """Does this plugin support writing operations for the given protocol?
 
@@ -77,8 +65,8 @@ class SmartOpenFileSystemPlugin(object):
         given protocol.
         """
 
-    @_protocol_hookspec
-    def smart_open_supports_create(self, protocol):
+    @_hookspec
+    def supports_create(self, protocol):
         # type: (str) -> bool
         """Does this plugin support creating files using the given protocol?
 
@@ -98,8 +86,8 @@ class SmartOpenFileSystemPlugin(object):
         given protocol.
         """
 
-    @_protocol_hookspec
-    def smart_open_supports_exclusive_create(self, protocol):
+    @_hookspec
+    def supports_exclusive_create(self, protocol):
         # type: (str) -> bool
         """Does this plugin support exclusive creation of files for the given
         protocol?
@@ -121,8 +109,8 @@ class SmartOpenFileSystemPlugin(object):
         using the given protocol.
         """
 
-    @_protocol_hookspec
-    def smart_open_open_binary_stream(self, uri, mode, buffering=-1, **kwargs):
+    @_hookspec
+    def open_binary_stream(self, uri, mode, buffering=-1, **kwargs):
         # type: (Uri, IOMode, int, ...) -> bool
         """Open the given resource and return a binary file stream.
 
@@ -153,20 +141,8 @@ class SmartOpenFileSystemPlugin(object):
 class SmartOpenCompressionPlugin(object):
     """Base class for all plugins implementing a compression codec."""
 
-    @_compression_hookspec
-    def smart_open_implemented_compression_algorithms(self):
-        # type: () -> Union[str, Iterable[str]]
-        """Declare what compression algorithms this plugin supports.
-
-        Returns
-        -------
-
-        A string or iterable of strings of the file extensions that this plugin
-        recognizes.
-        """
-
-    @_compression_hookspec
-    def smart_open_supports_codec(self, uri, stream):
+    @_hookspec
+    def supports_codec(self, uri, stream):
         # type: (Uri, BinaryIO) -> bool
         """Determine if this plugin supports the compression codec for the given
         URI and/or file stream.
@@ -188,21 +164,78 @@ class SmartOpenCompressionPlugin(object):
         file is compressed with.
         """
 
-    @compression_hook(hookwrapper=True)
-    def smart_open_supports_codec(self, uri, stream):
-        # type: (Uri, BinaryIO) -> Iterator[None]
-        """A wrapper for the :meth:`smart_open_supports_codec` hook to ensure the
-        stream pointer isn't moved after an implementation returns.
-        """
-        try:
-            initial_position = stream.tell()
-        except AttributeError:
-            # Stream doesn't support tell() so we have no way of resetting the
-            # stream pointer to where it was once the plugins return.
-            yield
-            return
 
-        try:
-            yield
-        finally:
-            stream.seek(initial_position)
+def class_name(klass):
+    # type: (type) -> str
+    """Get the full import path of a class as defined in the entry point providing it.
+
+    Parameters
+    ----------
+
+    klass: type
+        Any class object. Not an instance of the class!
+
+    Returns
+    -------
+
+    The full import path to the module where possible, followed by a colon, and
+    the name of the class. On Python 3 this will be the fully-qualified name of
+    the class in case it's nested.
+    """
+    return klass.__module__ + ":" + getattr(klass, "__qualname__", klass.__name__)
+
+
+class PluginConflictWarning(UserWarning):
+    """This plugin implements a protocol or compression algorithm that another plugin
+    already implements."""
+
+    def __init__(self, protocol, plugin_kind, existing_class, conflicting_class):
+        # type: (str, str, type, type) -> None
+        super(PluginConflictWarning, self).__init__(
+            "%s %r already implemented by plugin %s; ignoring plugin %s"
+            % (
+                plugin_kind.capitalize(),
+                protocol,
+                class_name(existing_class),
+                class_name(conflicting_class),
+            )
+        )
+
+
+def discover_plugins(entry_point, plugin_kind):
+    # type: (str, str) -> Dict[str, Any]
+    """
+
+    Parameters
+    ----------
+
+    entry_point: str
+        The second half of the entry point namespace. It should be a valid Python
+        identifier.
+
+    plugin_kind: str
+        A human-readable string indicating the kind of plugin this function is loading,
+        e.g. "protocol" or "compression codec". Only used in error messages.
+
+    Returns
+    -------
+
+    """
+    unloaded_plugins = {}
+
+    for entry_point in pkg_resources.iter_entry_points("smart_open." + entry_point):
+        name = entry_point.name
+        plugin_class = entry_point.load()
+
+        if entry_point.name in unloaded_plugins:
+            warnings.warn(
+                PluginConflictWarning(
+                    entry_point.name, plugin_kind, unloaded_plugins[name], plugin_class
+                )
+            )
+            continue
+        unloaded_plugins[name] = plugin_class
+
+    return {
+        name: plugin_class() for name, plugin_class in six.iteritems(unloaded_plugins)
+    }
